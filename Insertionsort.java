6d8ef19e1df0e6407e8c7a4e2a94396fd376cc1b
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-07-12 21:43:58 +0530

    Add files via upload
    sorting Algorithm

---
+++ Insertionsort.java
@@ -0,0 +1,16 @@
+package Sorting;
+
+public class Insertionsort {
+    public static void Insertion(int arr[]){
+
+    }
+    public static void print(int arr[]){
+for(int i = 0; i<arr.length; i++){
+    System.out.print(arr[i]+" ");
+}
+        System.out.println();
+    }
+    public static void main(String args []){
+        int arr[] = {5,4,1,3,2};
+    }
+}
